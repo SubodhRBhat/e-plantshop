0b32282886ac99e3819d74f3853b564ee3f1ad76
Subodh R Bhat <subodhrbhat123@gmail.com>
2024-12-15 17:48:23 +0530

    added

---
+++ src/AboutUs.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      {/* <h1 className="about-us-heading">About Us</h1> */}
+      <p className="about-us-description">Welcome to Planting with Subodh</p>
+      <p className="about-us-content">
+        At Planting with Subodh, we believe in growing more than just plants.  We are cultivating joy, sustainability and a connection to nature.
+      </p>
+      {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
+      <p className="about-us-content">
+       We are here to make plant care approachable and rewarding.  Whether you're a seasoned or a first-time gardener, our goal is to support your journey to greener living.
+      </p>
+      {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
+
+      <p className="about-us-content">
+        Join us in our mission to create better,greener world and secure a future for the next generations. Visit Planting with Subodh today.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
